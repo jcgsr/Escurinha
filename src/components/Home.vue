55532ed8f99ed0837b766265f5a6c0e57be08699
jcgsr <cabralguerra@gmail.com>
2020-10-26 01:29:09 -0300

    a tag id anchor

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,27 +3,27 @@
     <img :src="require('../assets/Escurinha-topo.png')" />
     <div class="fotos-menu">
 
-     <div class="links"> 
-       <router-link :to="{path: '/sobre', hash: '#acupuntura'}">
-         <img :src="require('../assets/Escurinha-acupuntura.jpeg')" />
-       </router-link>      
-       <div class="top-right mb-3">Como funciona a acupuntura?</div>
-     </div>
+     <div class="links mt-5"> 
+      <a href="/sobre/#acupuntura">        
+       <img :src="require('../assets/Escurinha-acupuntura.jpeg')" />
+     </a>    
+     <div class="top-right mb-3">Como funciona a acupuntura?</div>
+   </div>
 
-     <div class="links">          
-      <router-link to="/sobre">  
-        <img :src="require('../assets/Escurinha-pilates.jpeg')" />
-      </router-link>
-      <div class="top-right mb-3">Por que devo fazer pilates?</div>
-    </div>
-
-    <div class="links">          
-      <router-link to="/sobre">  
-        <img :src="require('../assets/Escurinha-fisio.jpeg')" />
-      </router-link>
-      <div class="top-right mb-3">Quando devo procurar um fisio?</div>
-    </div>   
+   <div class="links">          
+    <a href="/sobre/#pilates">
+      <img :src="require('../assets/Escurinha-pilates.jpeg')" />
+    </a>
+    <div class="top-right mb-3">Por que devo fazer pilates?</div>
   </div>
+
+  <div class="links">          
+    <a href="/sobre/#fisio">
+      <img :src="require('../assets/Escurinha-fisio.jpeg')" />
+    </a>
+    <div class="top-right mb-3">Quando devo procurar um fisio?</div>
+  </div>   
+</div>
 </div>
 
 </template>
@@ -65,7 +65,7 @@ img {
 }
 
 @media only screen and (max-width: 456px) {
-  
+
   .fotos-menu img {
     margin-bottom: .5rem;
     margin-left: .5rem;
